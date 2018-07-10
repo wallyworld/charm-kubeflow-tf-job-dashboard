a9ddc8662fc2c5dfc23f1207aee1f5af1d060dba
Cory Johns <johnsca@gmail.com>
2018-07-10 18:28:14 -0400

    More accurate status messages
    The active status for k8s charms should be set by Juju itself once the
    containers were successfully created.

--- reactive/kubeflow_tf_job_dashboard.py
+++ reactive/kubeflow_tf_job_dashboard.py
@@ -9,7 +9,7 @@ from charms.layer.basic import pod_spec_set
 
 @when_not('charm.kubeflow-tf-job-dashboard.started')
 def start_charm():
-    layer.status.maintenance('configuring tf-job-dashboard container')
+    layer.status.maintenance('configuring container')
 
     pod_spec_set(yaml.dump({
         'containers': [
@@ -30,5 +30,5 @@ def start_charm():
         ],
     }))
 
-    layer.status.active('ready')
+    layer.status.maintenance('creating container')
     set_flag('charm.kubeflow-tf-job-dashboard.started')
